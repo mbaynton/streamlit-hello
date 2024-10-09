cbf0d83dce321e4d217b29c84fd2b1d754e35789
Mike Baynton <mike@mbaynton.com>
2024-10-09 16:09:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,10 @@ Hello *everybody!*
 
 Thanks for using Posit Connect Cloud!
 """)
+
+st.write("## Headers")
+st.write(st.context.headers)
+st.write("## Cookies")
+st.write(st.context.cookies)
+st.write("## Query parameters")
+st.write(st.query_params)
